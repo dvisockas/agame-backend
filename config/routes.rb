e1b2ae8dbb57174224c790b0388755395fb97ace
LukKair <lukas@wisemonks.com>
2015-09-26 18:21:56 +0300

    only

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   namespace :v1, { defaults: { format: :json } } do
 
-    resources :players, only: [:index, :create, :update, :show]
-    resources :estates, only: [:index, :create, :update, :show]
-    resources :estate_types, only: [:index, :create, :update, :show]
+    resources :players
+    resources :estates
+    resources :estate_types
     resources :game, only: :index
 
   end
