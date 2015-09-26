cf186617752f71f77eee3264b6cc18a7dfe7cb54
LukKair <lukas@wisemonks.com>
2015-09-26 18:17:02 +0300

    rack cors

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module AgameBackend
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :put, :delete]
       end
     end
   end
