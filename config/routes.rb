7ff46e43162c41d381c67cbe0447332af0792a1c
LukKair <lukas@wisemonks.com>
2015-09-27 13:23:40 +0300

    oi

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   namespace :v1, { defaults: { format: :json } } do
 
     resources :players, except: [:new, :edit] do
+      patch :attack
       member do
-        patch :attack
         patch :refuse_challenge
       end
     end
